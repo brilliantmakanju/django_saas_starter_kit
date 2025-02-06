1b59fc201a51bc3f1615d5c961e63f4be2fee551
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-02-06 19:24:26 +0000

    chore: added vercel host url to allowed in settings

--- saas_staterKit/settings.py
+++ saas_staterKit/settings.py
@@ -64,7 +64,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = ['discrete-sacred-bobcat.ngrok-free.app', 'localhost', '127.0.0.1', "*"]
+ALLOWED_HOSTS = ['discrete-sacred-bobcat.ngrok-free.app', 'django-saas-starter-kit.vercel.app', 'localhost', '127.0.0.1', "*"]
 
 # Application definitions
 # **SHARED_APPS** is for apps that will be shared across all tenants.
