044b041640cd224385653bfc75adda54d9aa3d53
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-02-06 19:04:53 +0000

    chore: updated the vercel json file changing python version.

--- build.sh
+++ build.sh
@@ -2,16 +2,16 @@ echo "🚀 Starting Render Build Process..."
 
 # Install dependencies
 echo "📦 Installing dependencies..."
-python -m pip install --upgrade pip
-python -m pip install -r requirements.txt
+python3.9 -m pip install --upgrade pip
+python3.9 -m pip install -r requirements.txt
 
 # Apply database migrations
 echo "🛠 Applying database migrations..."
-python manage.py migrate --noinput
+python3.9 manage.py migrate --noinput
 
 # Collect static files
 echo "🎨 Collecting static files..."
-python manage.py collectstatic --noinput
+python3.9 manage.py collectstatic --noinput
 
 # Start the server
 echo "✅ Build process completed. Ready to deploy!"
