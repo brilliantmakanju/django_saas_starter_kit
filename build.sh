66ee7b30a3fe5c23276ee67a6bd09504bd0a1252
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-02-06 19:10:59 +0000

    chore: updated the vercel json file with the python path to match the library installation.

--- build.sh
+++ build.sh
@@ -2,19 +2,19 @@ echo "🚀 Starting Render Build Process..."
 
 # Install dependencies
 echo "📦 Installing dependencies..."
-python3.13 -m venv viper
+python3.12 -m venv viper
 source viper/bin/activate
-python3.13 -m pip install --upgrade pip
-python3.13 -m pip install -r requirements.txt
+python3.12 -m pip install --upgrade pip
+python3.12 -m pip install -r requirements.txt
 
 
 # Apply database migrations
 echo "🛠 Applying database migrations..."
-python3.13 manage.py migrate --noinput
+python3.12 manage.py migrate --noinput
 
 # Collect static files
 echo "🎨 Collecting static files..."
-python3.13 manage.py collectstatic --noinput
+python3.12 manage.py collectstatic --noinput
 
 # Start the server
 echo "✅ Build process completed. Ready to deploy!"
