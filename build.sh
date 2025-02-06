fa8cf789915f415304a788e0ae601ffc4f26525e
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-02-06 18:51:57 +0000

    chore: build config for vercel.

--- build.sh
+++ build.sh
@@ -7,16 +7,16 @@ echo "🚀 Starting Render Build Process..."
 
 # Install dependencies
 echo "📦 Installing dependencies..."
-pip install --upgrade pip
-pip install -r requirements.txt
+python3.13.1 -m pip install --upgrade pip
+python3.13.1 -m pip install -r requirements.txt
 
 # Apply database migrations
 echo "🛠 Applying database migrations..."
-python manage.py migrate --noinput
+python3.13.1 manage.py migrate --noinput
 
 # Collect static files
 echo "🎨 Collecting static files..."
-python manage.py collectstatic --noinput
+python3.13.1 manage.py collectstatic --noinput
 
 # Start the server
 echo "✅ Build process completed. Ready to deploy!"
