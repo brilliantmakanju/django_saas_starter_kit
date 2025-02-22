60261f0ee3ef96958f57118a097a370e0828051e
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-02-22 12:09:00 +0000

    chore: apschedular config update.

--- core/apps.py
+++ core/apps.py
@@ -1,6 +1,14 @@
 from django.apps import AppConfig
+import threading
 
 
 class CoreConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "core"
+
+    def ready(self):
+        from .tasks import start_scheduler  # Import inside the function to prevent early execution
+
+        # Run scheduler in a separate thread
+        scheduler_thread = threading.Thread(target=start_scheduler, daemon=True)
+        scheduler_thread.start()
