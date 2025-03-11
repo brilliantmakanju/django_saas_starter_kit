344328cd42c1a3623c7c9d7995853a1d4fc0b026
Brilliant Makanju <brilliantmakanju4@gmail.com>
2025-03-11 18:43:17 +0000

    chore: updated url to render iamges for now, will be moving to cloudinary later.

--- saas_staterKit/urls.py
+++ saas_staterKit/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
+from django.urls import re_path
 from django.conf import settings
+from django.views.static import serve
 from django.urls import path, include
 from django.conf.urls.static import static
 
@@ -32,6 +34,11 @@ urlpatterns = [
 
 
 # Serve media files during development
-# if settings.DEBUG:
-# removed for now.
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# Serve media files in production manually
+if settings.DEBUG is False:  # Production mode
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
+    ]
